80b1112f30a2b9ff2f9035c7cec4f4155775f193
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2012-01-27 11:43:00 +0100

    Helpers updated:   new types of declinations     single/plural     male / female   Added new funcion yesno:     parameters management     default option   Added new function fyesno:     options sended with "string" but more fast   Deleted old function yes_no     the old function is not compatible with the new standard of translation

--- lib/yesno/helpers.rb
+++ lib/yesno/helpers.rb
@@ -1,3 +1,26 @@
-def yes_no(type, to_validate)
-  return to_validate ? I18n.t("yesno.#{type}.t") : I18n.t("yesno.#{type}.f")
+def yesno(to_validate, options = {})
+  defaults = {
+    :type => "default",
+    :pluralize => false,
+    :female => false
+  }
+  options = defaults.merge(options)
+  if options[:pluralize] == false
+    if options[:female] == false
+      return to_validate ? I18n.t("yesno.#{options[:type]}.smt") : I18n.t("yesno.#{options[:type]}.smf")
+    else
+      return to_validate ? I18n.t("yesno.#{options[:type]}.sft") : I18n.t("yesno.#{options[:type]}.sff")
+    end
+  else
+    if options[:female] == false
+      return to_validate ? I18n.t("yesno.#{options[:type]}.pmt") : I18n.t("yesno.#{options[:type]}.pmf")
+    else
+      return to_validate ? I18n.t("yesno.#{options[:type]}.pft") : I18n.t("yesno.#{options[:type]}.pff")
+    end
+  end
 end
+
+def fyesno(to_validate, type = "default", options = "sm")
+  return to_validate ? I18n.t("yesno.#{type}.#{options}t") : I18n.t("yesno.#{type}.#{options}f")
+end
+
